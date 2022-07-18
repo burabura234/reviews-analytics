b4978fad15860a06c942f4e657d52c521dcd32ea
Eric Yang <xsw26845@gmail>
2022-07-18 14:32:02 +0800

    add word count

--- read.py
+++ read.py
@@ -8,16 +8,50 @@ with open('reviews.txt', 'r') as f:
  		    print(len(data))
 print('檔案讀取完了，總共有:', len(data), '筆資料!')
 
-sum_len = 0
-for d in data:
-	sum_len += len(d)
-	print(sum_len)
-print('留言平均長度是', sum_len / len(data))
+# sum_len = 0
+# for d in data:
+# 	sum_len += len(d)
+# 	print(sum_len)
+# print('留言平均長度是', sum_len / len(data))
 
-new = []
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '比留言長度小於100')
+# print(new[0])
+# print(new[1])
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言資料含有good')
+# print(good[0])
+
+wc = {}
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '比留言長度小於100')
-print(new[0])
-print(new[1])
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+   		break
+	if word in wc:
+		print(word, '出現次數為', wc[word])
+	else:
+		print('這個字沒有出現過喔!')
+
+print('感謝使用本查詢功能')
